482ba01cb3b69feae3fa537c3e96a797fd8e7458
oey192 <oeystanger@gmail.com>
2013-04-01 16:56:35 -0600

    Attempted to programmatically create a line - doesn't work yet

--- JECO - esisneros/Map.pas
+++ JECO - esisneros/Map.pas
@@ -15,6 +15,8 @@ type
     Image2: TImage;
     ImageViewer1: TImageViewer;
     Blero: TStyleBook;
+    procedure drawLine(x1, y1, x2, y2 : Integer);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -27,5 +29,32 @@ var
 implementation
 
 {$R *.fmx}
+
+uses JECOMap;
+
+procedure TMapForm.drawLine(x1, y1, x2, y2 : Integer);
+var line : TLine;
+begin
+
+line := TLine.Create(self);
+
+line.Stroke := TStrokeBrush.Create(TBrushKind.bkSolid, $0000F0);
+
+line := TLine.Create(application.Owner);
+line.SetBounds(x1, y1, x2-x1, y2-y1);
+line.Stroke := TStrokeBrush.Create(TBrushKind.bkSolid, $0000F0);
+//line.Stroke.Kind := TBrushKind.bkSolid;
+//line.Stroke.Color := $0000F0;
+line.StrokeThickness := 20;
+line.Visible := true;
+line.Opacity := 1;
+line.BringToFront;
+application.ProcessMessages;
+end;
+
+procedure TMapForm.FormCreate(Sender: TObject);
+begin
+  drawLine(0, 0, 40, 60);
+end;
 
 end.

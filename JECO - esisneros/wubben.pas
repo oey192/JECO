1e62b850c5c2c2b9ff89a37dd045cb887ec62ef4
Orion Stanger <oeystanger@gmail.com>
2013-04-09 02:32:06 -0600

    changed timage to timageviewer

--- JECO - esisneros/wubben.pas
+++ JECO - esisneros/wubben.pas
@@ -5,11 +5,10 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Rtti, System.Classes,
   System.Variants, FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.Objects,
-  JECOMap;
+  JECOMap, FMX.Layouts, FMX.ExtCtrls;
 
 type
   TwubbenScience = class(TForm)
-    Image1: TImage;
     Line1: TLine;
     Line2: TLine;
     Line3: TLine;
@@ -54,11 +53,11 @@ type
     Line42: TLine;
     Line43: TLine;
     Button1: TButton;
-    Image2: TImage;
-    Image3: TImage;
     StyleBook1: TStyleBook;
-    procedure hideLines;
+    ImageViewer1: TImageViewer;
+    procedure setLineVisibility(tf : boolean);
     procedure showPath(start, finish : String);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -72,159 +71,164 @@ implementation
 
 {$R *.fmx}
 
-procedure Twubbenscience.hideLines;
+procedure TwubbenScience.FormCreate(Sender: TObject);
 begin
-  Line1.Visible := false;
   Line1.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line1.StrokeThickness := 5;
-  Line2.Visible := false;
   Line2.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line2.StrokeThickness := 5;
-  Line3.Visible := false;
   Line3.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line3.StrokeThickness := 5;
-  Line4.Visible := false;
   Line4.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line4.StrokeThickness := 5;
-  Line5.Visible := false;
   Line5.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line5.StrokeThickness := 5;
-  Line6.Visible := false;
   Line6.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line6.StrokeThickness := 5;
-  Line7.Visible := false;
   Line7.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line7.StrokeThickness := 5;
-  Line8.Visible := false;
   Line8.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line8.StrokeThickness := 5;
-  Line9.Visible := false;
   Line9.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line9.StrokeThickness := 5;
-  Line10.Visible := false;
   Line10.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line10.StrokeThickness := 5;
-  Line11.Visible := false;
   Line11.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line11.StrokeThickness := 5;
-  Line12.Visible := false;
   Line12.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line12.StrokeThickness := 5;
-  Line13.Visible := false;
   Line13.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line13.StrokeThickness := 5;
-  Line14.Visible := false;
   Line14.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line14.StrokeThickness := 5;
-  Line15.Visible := false;
   Line15.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line15.StrokeThickness := 5;
-  Line16.Visible := false;
   Line16.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line16.StrokeThickness := 5;
-  Line17.Visible := false;
   Line17.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line17.StrokeThickness := 5;
-  Line18.Visible := false;
   Line18.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line18.StrokeThickness := 5;
-  Line19.Visible := false;
   Line19.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line19.StrokeThickness := 5;
-  Line20.Visible := false;
   Line20.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line20.StrokeThickness := 5;
-  Line21.Visible := false;
   Line21.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line21.StrokeThickness := 5;
-  Line22.Visible := false;
   Line22.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line22.StrokeThickness := 5;
-  Line23.Visible := false;
   Line23.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line23.StrokeThickness := 5;
-  Line24.Visible := false;
   Line24.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line24.StrokeThickness := 5;
-  Line25.Visible := false;
   Line25.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line25.StrokeThickness := 5;
-  Line26.Visible := false;
   Line26.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line26.StrokeThickness := 5;
-  Line27.Visible := false;
   Line27.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line27.StrokeThickness := 5;
-  Line28.Visible := false;
   Line28.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line28.StrokeThickness := 5;
-  Line29.Visible := false;
   Line29.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line29.StrokeThickness := 5;
-  Line30.Visible := false;
   Line30.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line30.StrokeThickness := 5;
-  Line31.Visible := false;
   Line31.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line31.StrokeThickness := 5;
-  Line32.Visible := false;
   Line32.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line32.StrokeThickness := 5;
-  Line33.Visible := false;
   Line33.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line33.StrokeThickness := 5;
-  Line33.Visible := false;
-  Line33.Stroke.Color := TAlphaColorRec.Deepskyblue;
-  Line33.StrokeThickness := 5;
-  Line34.Visible := false;
   Line34.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line34.StrokeThickness := 5;
-  Line35.Visible := false;
   Line35.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line35.StrokeThickness := 5;
-  Line36.Visible := false;
   Line36.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line36.StrokeThickness := 5;
-  Line37.Visible := false;
   Line37.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line37.StrokeThickness := 5;
-  Line38.Visible := false;
   Line38.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line38.StrokeThickness := 5;
-  Line39.Visible := false;
   Line39.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line39.StrokeThickness := 5;
-  Line40.Visible := false;
   Line40.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line40.StrokeThickness := 5;
-  Line41.Visible := false;
   Line41.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line41.StrokeThickness := 5;
-  Line42.Visible := false;
   Line42.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line42.StrokeThickness := 5;
-  Line43.Visible := false;
   Line43.Stroke.Color := TAlphaColorRec.Deepskyblue;
   Line43.StrokeThickness := 5;
+  setLineVisibility(false);
+end;
+
+procedure Twubbenscience.setLineVisibility(tf : boolean);
+begin
+  Line1.Visible := tf;
+  Line2.Visible := tf;
+  Line3.Visible := tf;
+  Line4.Visible := tf;
+  Line5.Visible := tf;
+  Line6.Visible := tf;
+  Line7.Visible := tf;
+  Line8.Visible := tf;
+  Line9.Visible := tf;
+  Line10.Visible := tf;
+  Line11.Visible := tf;
+  Line12.Visible := tf;
+  Line13.Visible := tf;
+  Line14.Visible := tf;
+  Line15.Visible := tf;
+  Line16.Visible := tf;
+  Line17.Visible := tf;
+  Line18.Visible := tf;
+  Line19.Visible := tf;
+  Line20.Visible := tf;
+  Line21.Visible := tf;
+  Line22.Visible := tf;
+  Line23.Visible := tf;
+  Line24.Visible := tf;
+  Line25.Visible := tf;
+  Line26.Visible := tf;
+  Line27.Visible := tf;
+  Line28.Visible := tf;
+  Line29.Visible := tf;
+  Line30.Visible := tf;
+  Line31.Visible := tf;
+  Line32.Visible := tf;
+  Line33.Visible := tf;
+  Line34.Visible := tf;
+  Line35.Visible := tf;
+  Line36.Visible := tf;
+  Line37.Visible := tf;
+  Line38.Visible := tf;
+  Line39.Visible := tf;
+  Line40.Visible := tf;
+  Line41.Visible := tf;
+  Line42.Visible := tf;
+  Line43.Visible := tf;
 end;
 
 procedure Twubbenscience.showPath(start, finish: String);
 var i, index, startNum, finishNum : Integer;
 begin
 
-hideLines;
+//if (start = '') or (finish = '') then
+
+
+setLineVisibility(false);
 
 index := -1;
 for i := 1 to Length(finish) do
   if (finish[i] > '0') and (finish[i] < '9') and (index = -1) then
     index := i;
 
-  if finish[index] = '1' then
+  {if finish[index] = '1' then
     Image1.BringToFront
   else if finish[index] = '2' then
     Image2.BringToFront
   else if finish[index] = '3' then
-    Image3.BringToFront;
+    Image3.BringToFront;}
 
   //add code to make following apply to first floor only
 

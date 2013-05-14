ddb141e3f34e71551282baf7515a06ba388d7fe8
Christina Reed <daichitenskirpg@gmail.com>
2013-05-14 09:43:38 -0600

    Schedule Form Updated
    Not complete but functional and more uniformed look to the rest of the
    forms.

--- JECO - esisneros/Schedule.pas
+++ JECO - esisneros/Schedule.pas
@@ -9,20 +9,15 @@ uses
 
 type
   TForm2 = class(TForm)
-    Panel3: TPanel;
     btnQuit: TButton;
     btnViewSchMap: TButton;
     btnHome: TButton;
     Panel1: TPanel;
     getDirs: TButton;
     Label3: TLabel;
-    CMU_Logo: TImage;
-    CMU_Montrose_Logo: TImage;
-    WCCC_Logo: TImage;
     title_CMUScheduleAndProfile: TLabel;
     JECO_Logo: TImage;
     Copyright_footer: TLabel;
-    Title_ChooseCampus: TLabel;
     label_professor: TLabel;
     Label6: TLabel;
     btnRemove: TButton;
@@ -33,7 +28,6 @@ type
     colProfessor: TStringColumn;
     coursePrefix: TComboBox;
     courseSuffix: TComboBox;
-    professorList: TComboBox;
     Label_PPass: TLabel;
     PPass_ComboBox: TComboBox;
     PP_G24: TListBoxItem;
@@ -64,29 +58,20 @@ type
     c130: TListBoxItem;
     c196: TListBoxItem;
     c206: TListBoxItem;
-    ektare: TListBoxItem;
-    kcastlet: TListBoxItem;
-    lpayne: TListBoxItem;
-    grader: TListBoxItem;
-    wmacevoy: TListBoxItem;
-    jddobbs: TListBoxItem;
+    sideMenu_background: TImage;
+    Panel3: TPanel;
+    campus_option: TComboBox;
+    Label2: TLabel;
+    professorList: TComboBox;
+    ListBoxItem2: TListBoxItem;
     procedure getDirsClick(Sender: TObject);
     procedure btnHomeClick(Sender: TObject);
     procedure btnQuitClick(Sender: TObject);
     procedure btnRemoveClick(Sender: TObject);
     procedure btnViewSchMapClick(Sender: TObject);
-    procedure CMU_LogoClick(Sender: TObject);
-    procedure CMU_Montrose_LogoClick(Sender: TObject);
-    procedure WCCC_LogoClick(Sender: TObject);
-    procedure FormShow(Sender: TObject);
-    procedure CMU_Montrose_LogoMouseEnter(Sender: TObject);
-    procedure CMU_Montrose_LogoMouseLeave(Sender: TObject);
-    procedure CMU_LogoMouseEnter(Sender: TObject);
-    procedure CMU_LogoMouseLeave(Sender: TObject);
-    procedure WCCC_LogoMouseEnter(Sender: TObject);
-    procedure WCCC_LogoMouseLeave(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
-  private
+    procedure FormCreate(Sender: TObject);
+    private
     { Private declarations }
   public
     { Public declarations }
@@ -94,8 +79,16 @@ type
 
 //type Campus_Selections = (NO_CAMPUS, CMU, WCCC, Montrose);
 
+  Stype = record
+      courseName : string[5];
+      courseNum : integer;
+      sectNum : integer;
+      professor : string[15];
+  end;
+  ClassArray = array[1..10] of Stype;
 var
   Form2: TForm2;
+  inf, outf : textfile;
  // Campus : Campus_Selections;
   schedule_row : integer;
   schedule_column : integer;
@@ -119,7 +112,7 @@ end;
 
 procedure TForm2.btnRemoveClick(Sender: TObject);
 begin
-  if scheduleGrid.RowCount <> 1 then
+  if scheduleGrid.RowCount > 1 then
   begin
     scheduleGrid.RowCount := scheduleGrid.RowCount - 1;
   end
@@ -136,96 +129,52 @@ begin
   MapForm.Visible := true;
 end;
 
-procedure TForm2.CMU_LogoClick(Sender: TObject);
-begin
-  CMU_Logo.Opacity := 1.0;
-  WCCC_Logo.Opacity := 0.4;
-  CMU_Montrose_Logo.Opacity := 0.4;
-  Campus := CMU;
-end;
-
-procedure TForm2.CMU_LogoMouseEnter(Sender: TObject);
-begin
-  CMU_Logo.Opacity := 1.0;
-end;
-
-procedure TForm2.CMU_LogoMouseLeave(Sender: TObject);
-begin
-if Campus <> CMU then
-    CMU_Logo.Opacity := 0.4;
-end;
-
-procedure TForm2.CMU_Montrose_LogoClick(Sender: TObject);
-begin
-  CMU_Logo.Opacity := 0.4;
-  WCCC_Logo.Opacity := 0.4;
-  CMU_Montrose_Logo.Opacity := 1.0;
-  Campus := Montrose;
-end;
-
-procedure TForm2.CMU_Montrose_LogoMouseEnter(Sender: TObject);
-begin
-  CMU_Montrose_Logo.Opacity := 1.0;
-end;
-
-procedure TForm2.CMU_Montrose_LogoMouseLeave(Sender: TObject);
-begin
-  if Campus <> Montrose then
-    CMU_Montrose_Logo.Opacity := 0.4;
-end;
-
-procedure TForm2.WCCC_LogoClick(Sender: TObject);
-begin
-  CMU_Logo.Opacity := 0.4;
-  WCCC_Logo.Opacity := 1.0;
-  CMU_Montrose_Logo.Opacity := 0.4;
-  Campus := WCCC;
-end;
-
-procedure TForm2.WCCC_LogoMouseEnter(Sender: TObject);
-begin
-  WCCC_Logo.Opacity := 1.0;
-end;
-
-procedure TForm2.WCCC_LogoMouseLeave(Sender: TObject);
-begin
-if Campus <> WCCC then
-    WCCC_Logo.Opacity := 0.4;
-end;
-
 procedure TForm2.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
   application.Terminate;
 end;
 
-procedure TForm2.FormShow(Sender: TObject);
+procedure popProf(Sender : TObject; professorList: TComboBox; coursePrefix: TComboBox);
+var
+  str : string[15];
 begin
-   Campus := JECOMap.Campus;
-  if Campus = CMU then
+  professorList.clear;
+  if  coursePrefix.Selected.Text = 'MATH' then
   begin
-      CMU_Logo.Opacity := 1.0;
-      WCCC_Logo.Opacity := 0.4;
-      CMU_Montrose_Logo.Opacity := 0.4;
+    assignfile(inf,'schedule/CMU/MATHstaff.txt');
+    reset(inf);
+    while not eof(inf) do
+      begin
+        readln(inf,str);
+        professorList.Items.Add(str);
+      end;
   end
-  else if Campus = WCCC then
+  else if coursePrefix.selected.Text = 'CSCI' then
   begin
-      CMU_Logo.Opacity := 0.4;
-      WCCC_Logo.Opacity := 1.0;
-      CMU_Montrose_Logo.Opacity := 0.4;
-  end
-  else if Campus = Montrose then
-  begin
-      CMU_Logo.Opacity := 0.4;
-      WCCC_Logo.Opacity := 0.4;
-      CMU_Montrose_Logo.Opacity := 1.0;
-  end
-  else
-  begin
-      CMU_Logo.Opacity := 1.0;
-      WCCC_Logo.Opacity := 0.4;
-      CMU_Montrose_Logo.Opacity := 0.4;
-      Campus := CMU;
+    assignfile(inf,'schedule/CMU/CSCIstaff.txt');
+    reset(inf);
+    while not eof(inf) do
+      begin
+        readln(inf,str);
+        professorList.Items.Add(str);
+      end;
   end;
+   closefile(inf);
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+var
+  str : string[15];
+begin
+  coursePrefix.Clear;
+  assignfile(inf,'schedule/CMU/index.txt');
+  reset(inf);
+  while not eof(inf) do
+    begin
+      readln(inf,str);
+      coursePrefix.Items.Add(str);
+    end;
+   closefile(inf);
 end;
 
 procedure TForm2.getDirsClick(Sender: TObject);
@@ -261,5 +210,6 @@ end;
   scheduleGrid.Cells[1, scheduleGrid.Selected] := professorList.Items[professorList.ItemIndex];
   scheduleGrid.Selected := scheduleGrid.Selected + 1;
 end;
+
 
 end.
